2ce81afec78b739a728a0b7110438cfd11ddb370
mazharibnazahur <mazhar.zahur@dohatec.com.bd>
2023-08-18 13:56:55 +0600

    rename custom message properties

--- src/main/java/org/csekuaa/backend/util/config/MessageConfig.java
+++ src/main/java/org/csekuaa/backend/util/config/MessageConfig.java
@@ -1,5 +1,6 @@
 package org.csekuaa.backend.util.config;
 
+import java.util.Locale;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
@@ -9,35 +10,32 @@ import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
-import java.util.Locale;
-
 @Configuration
 public class MessageConfig implements WebMvcConfigurer {
 
-    private static final String ENCODING = "UTF-8";
+  private static final String ENCODING = "UTF-8";
 
-    @Bean
-    public ResourceBundleMessageSource messageSource() {
-        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasenames("i18n/messages", "ValidationMessages");
-        messageSource.setUseCodeAsDefaultMessage(true);
-        messageSource.setDefaultEncoding(ENCODING);
-        messageSource.setCacheSeconds(5);
-        return messageSource;
-    }
+  @Bean
+  public ResourceBundleMessageSource messageSource() {
+    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+    messageSource.setBasenames("i18n/customMessages", "ValidationMessages", "messages");
+    messageSource.setUseCodeAsDefaultMessage(true);
+    messageSource.setDefaultEncoding(ENCODING);
+    messageSource.setCacheSeconds(5);
+    return messageSource;
+  }
 
-    @Bean
-    public LocaleResolver localeResolver(){
-        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
-        localeResolver.setDefaultLocale(new Locale("en"));
-        return localeResolver;
-    }
+  @Bean
+  public LocaleResolver localeResolver() {
+    SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+    localeResolver.setDefaultLocale(Locale.ENGLISH);
+    return localeResolver;
+  }
 
-
-    @Override
-    public Validator getValidator() {
-        LocalValidatorFactoryBean validator = new LocalValidatorFactoryBean();
-        validator.setValidationMessageSource(messageSource());
-        return validator;
-    }
+  @Override
+  public Validator getValidator() {
+    LocalValidatorFactoryBean validator = new LocalValidatorFactoryBean();
+    validator.setValidationMessageSource(messageSource());
+    return validator;
+  }
 }
